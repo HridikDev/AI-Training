df69f0543378104a98c1b60e7421314cc2255016
Hridik Dev <hridikori@gmail.com>
2025-06-05 10:35:13 +0530

    First Commit

--- emo.py
+++ emo.py
@@ -1,5 +1,11 @@
 import cv2
 from deepface import DeepFace
+import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
 
 cap=cv2.VideoCapture(0)
 
@@ -11,6 +17,11 @@ while True:
     cv2.putText(frame, f'Emotion: {emotion}', (50,50), cv2.FONT_HERSHEY_SIMPLEX,1 ,(255,0,0),2)
 
     cv2.imshow("Emotion Recognition", frame)
+
+    if emotion != voices:
+        engine.say(f"I am {emotion}")
+        engine.runAndWait()
+        last_emotion = emotion
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
